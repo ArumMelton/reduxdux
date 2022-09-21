79e322aa2dc363f12f940931872eeb380d27a884
ArumMelton <103224098+ArumMelton@users.noreply.github.com>
2022-09-21 13:35:47 -0400

    finished refactor of comp and pages

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
-// TODO add the Redux hooks useDispatch and useSelector
+import { useDispatch, useSelector } from "react-redux";
 
 import Cart from "../components/Cart";
-import { useStoreContext } from "../utils/GlobalState";
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
@@ -16,9 +15,8 @@ import { idbPromise } from "../utils/helpers";
 import spinner from "../assets/spinner.gif";
 
 function Detail() {
-  // TODO useDispatch handles dispatch and useSelector handles state
-  // ? Are there any other places that we've used context? These will also need to be replaced in the same way.
-  const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
